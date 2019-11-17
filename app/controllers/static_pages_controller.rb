32518ec274309e5eb9fc4f38046b9d7f0ab3768a
EC2 Default User <ec2-user@ip-172-31-94-100.ec2.internal>
2019-11-17 10:56:42 +0000

    add-user-micropost

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,8 @@ class StaticPagesController < ApplicationController
   before_action :logged_in_user, only: [:home]
 
   def home
+    @micropost = current_user.microposts.build 
+    @feed_items = current_user.feed.paginate(page: params[:page])
   end
 
   def about
